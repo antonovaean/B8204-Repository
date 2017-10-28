1f48ee165f28f489aff01a2ff160311fc45c218c
ParaDogs <vakabakayo@yandex.ru>
2017-10-28 20:28:36 +1000

    Test

--- Tasks/Task0/SimakovValentin/fib/imp1.cpp
+++ Tasks/Task0/SimakovValentin/fib/imp1.cpp
@@ -10,11 +10,5 @@ int aplusb(int a, int b) {
     return b + a;
 }
 int fib(int n, int& c) {
-    return (int)((pow(((1 + sqrt(5)) / 2), n) - pow(((1 - sqrt(5)) / 2), n))/sqrt(5));
-    /*c++;
-    if (n < 1){
-        return 0;
-    } else if (n <= 2) {
-        return 1;
-    } else return fib(n - 1, c) + fib(n - 2, c);*/
+    //return (int)((pow(((1 + sqrt(5)) / 2), n) - pow(((1 - sqrt(5)) / 2), n))/sqrt(5)); 
 }
