356904d5e58eac03f77ec053f804ad475a169aa1
Valgulious <dmitryfedorenkof@gmail.com>
2017-11-11 12:10:43 +1000

    Удалил ненужные папки

--- Tasks/Task0/GolubKonstantin/fib.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by c0di on 27.10.2017.
-//
-
-#include "fib.h"
--- Tasks/Task0/SimakovValentin/fib/mylib.h
+++
@@ -1,8 +0,0 @@
-#ifndef _mylib
-#define _mylib 
-
-void sayhello();
-int aplusb(int a, int b);
-int fib(int n, int& c);
-
-#endif
