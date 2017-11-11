410758215ad980b9804e8c211581e21776c2635c
Григорий Сысоев <gregorysysoev@gmail.com>
2017-11-11 13:23:46 +1000

    Исправил ошибку

--- Tasks/Task0.2/lineNodeNoise2.c
+++ Tasks/Task0.2/lineNodeNoise2.c
@@ -2,7 +2,7 @@
 char* lineNodeNoise2(char *message; int messageLength) {
     char m = message[1];
     if (messageLength <= 7) {
-        message[1] = messag[5];
+        message[1] = message[5];
         message[5] = m;
     }
     return message;
