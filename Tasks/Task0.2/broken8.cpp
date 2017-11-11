ba0d8a68036d4f757a2f10e98299374890897213
mimiMileena <ren99999@mail.ru>
2017-11-11 12:54:03 +1000

    BlessRNG

---
+++ Tasks/Task0.2/broken8.cpp
@@ -0,0 +1,18 @@
+//
+//  broken8.cpp
+//  broken_telephone
+//
+//  Created by macbook on 11.11.2017.
+//  Copyright © 2017 macbook. All rights reserved.
+//
+
+#include "broken_telephone.h"
+#include <stdio.h>
+
+int char* lineNodeNoise8(char *message, int messageLength)
+{
+    char X = *message[3];
+*message[3]=*message[9];
+*message[9]=x;
+    return *message;
+}
