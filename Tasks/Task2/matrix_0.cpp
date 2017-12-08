41d8aecef5bd45df40e43abb57528ca1daa75c95
ParaDogs <vakabakayo@yandex.ru>
2017-12-08 22:19:07 +1000

    some changes

--- Tasks/Task2/matrix_0.cpp
+++ Tasks/Task2/matrix_0.cpp
@@ -1,7 +1,51 @@
 #include <stdio.h>
-#include "matrix_lib.h"
+#include <iostream>
+#include <ctime>
+
+class matrix{
+    private:
+        int rows, cols;
+        int** table;
+    public:
+        matrix(int n, int m){
+            rows = n; cols = m;
+            table = new int*[rows];
+            for (int i = 0; i < rows; i++)
+                table[i] = new int[cols];
+        }
+
+        void fill(int a, int b){
+            srand(time(NULL));
+            for(int i = 0; i < this->rows; i++)
+                for(int j = 0; j < this->cols; j++)
+                    this->table[i][j] = a + rand()%b;
+        }
+
+        void print(){
+            for(int i = 0; i < this->rows; i++){
+                for(int j = 0; j < this->cols; j++)
+                    printf("%d\t", this->table[i][j]);
+                printf("\n");
+            }
+            printf("\n");
+        }
+        /*matrix add(matrix m);
+        matrix mul(matrix m);
+        matrix inv(matrix m);
+        matrix trn();
+        int det;
+        void fill();
+        void print();*/
+};
+
 
 int main(){
+    matrix A(4, 5);
+    matrix B(4, 5);
+    A.fill(0, 90);
+    B.fill(0, 90);
+    A.print();
+    B.print();
 
     return 0;
 }
--- Tasks/Task2/matrix_lib.h
+++ Tasks/Task2/matrix_lib.h
@@ -1,6 +1,40 @@
-#ifndef PROJECT_MATRIX_H
-#define PROJECT_MATRIX_H
-namespace MatrixLib {
-    char* translit(char* message);
-}
-#endif //PROJECT_MATRIX_H
+#include <stdio.h>
+#include <iostream>
+#include <ctime>
+
+class matrix{
+    private:
+        int rows, cols;
+        int** table;
+    public:
+        matrix(int n, int m){
+            rows = n; cols = m;
+            table = new int*[rows];
+            for (int i = 0; i < rows; i++)
+                table[i] = new int[cols];
+        }
+
+        void fill(int a, int b){
+            srand(time(NULL));
+            for(int i = 0; i < this->rows; i++)
+                for(int j = 0; j < this->cols; j++)
+                    this->table[i][j] = a + rand()%b;
+        }
+
+        void print(){
+            for(int i = 0; i < this->rows; i++){
+                for(int j = 0; j < this->cols; j++)
+                    printf("%d\t", this->table[i][j]);
+                printf("\n");
+            }
+            printf("\n");
+        }
+        /*matrix add(matrix m);
+        matrix mul(matrix m);
+        matrix inv(matrix m);
+        matrix trn();
+        int det;
+        void fill();
+        void print();*/
+};
+
