88235e0c9da04fef9948c877a0ad9a878d57c06b
ParaDogs <vakabakayo@yandex.ru>
2017-12-08 19:54:12 +1000

    new task

---
+++ Tasks/Task2/matrix_0.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "matrix_lib.h"
+
+int main(){
+
+    return 0;
+}
---
+++ Tasks/Task2/matrix_lib.h
@@ -0,0 +1,6 @@
+#ifndef PROJECT_MATRIX_H
+#define PROJECT_MATRIX_H
+namespace MatrixLib {
+    char* translit(char* message);
+}
+#endif //PROJECT_MATRIX_H
