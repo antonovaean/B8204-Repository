7dd54afec49d6120863548e1ea6f41a83c2ea519
mimiMileena <ren99999@mail.ru>
2017-12-27 12:30:54 +1000

    Cleaned
    Cleaned repo before final work

--- Tasks/Task2/MM/determinant.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <iostream>
-#include <conio.h>
-#include <math.h>
-#include <ctime>
-using namespace std;
--- fib/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "mylib.h"
-#include <stdio.h>
-#include <math.h>
-
-int main() {
-    for (int i = 1; i < 35; i++) printf("%d ", fib(i)); 
-    return 0;
-}
